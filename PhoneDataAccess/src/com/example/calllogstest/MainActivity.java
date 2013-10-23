ebab3b48e8016ee3abb43edd391c48a4d0ba47c1
Darshan Desai <darshanBdesai@gmail.com>
2013-10-23 12:14:34 -0700

    Updated the PhoneDataAccess app to read the call logs from the phone and print it

--- PhoneDataAccess/src/com/example/calllogstest/MainActivity.java
+++ PhoneDataAccess/src/com/example/calllogstest/MainActivity.java
@@ -1,13 +1,65 @@
 package com.example.calllogstest;
 
+import java.util.Date;
+
 import android.app.Activity;
 import android.content.ContentResolver;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CallLog;
 import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.Menu;
+
+class RetrieveCallLogs {
+	ContentResolver cr = null;
+
+	public RetrieveCallLogs(ContentResolver cr1) {
+		cr = cr1;
+	}
+	
+	public void getCallLogs() {
+		Uri CALL_LOG_URI = CallLog.Calls.CONTENT_URI;
+		String NUMBER = CallLog.Calls.NUMBER;
+		String NAME = CallLog.Calls.CACHED_NAME;
+		String NUMBER_TYPE = CallLog.Calls.CACHED_NUMBER_TYPE; 
+		String CALL_TYPE = CallLog.Calls.TYPE;
+		String CALL_DATE = CallLog.Calls.DATE;
+		String CALL_DURATION = CallLog.Calls.DURATION;
+
+	    Cursor cur = cr.query(CALL_LOG_URI, null, null, null, null);
+	    if(cur != null && cur.getCount() >0) {
+		    while(cur.moveToNext()) {
+		    	String record = new String();
+			    String number = cur.getString(cur.getColumnIndex(NUMBER));
+			    String name = cur.getString(cur.getColumnIndex(NAME));
+			    String number_type = cur.getString(cur.getColumnIndex(NUMBER_TYPE));
+			    int call_type = Integer.parseInt(cur.getString(cur.getColumnIndex(CALL_TYPE)));
+			    Date call_date = new Date(Long.valueOf(
+			    		cur.getString(cur.getColumnIndex(CALL_DATE))));
+			    int call_duration = Integer.parseInt(cur.getString(cur.getColumnIndex(CALL_DURATION)))/60;
+			    String call_type_str = null;
+
+			    switch(call_type) {
+			    	case CallLog.Calls.OUTGOING_TYPE:
+			    		call_type_str = "Outgoing";
+			    		break;
+			    	case CallLog.Calls.INCOMING_TYPE:
+			    		call_type_str = "Incoming";
+			    		break;
+			    	case CallLog.Calls.MISSED_TYPE:
+			    		call_type_str = "Missed";
+			    		break;
+			    }
+			    record = name + " " + number + " (" + number_type + ") ";
+			    record = record + call_type_str + " " + call_date + " " + call_duration + " mins";
+			    Log.v("DD",record);
+		   }
+	   }
+	   cur.close();
+	}
+}
 
 class RetrieveContacts {
 	ContentResolver cr = null;
@@ -83,7 +135,6 @@ class RetrieveContacts {
 		   	cur.close();
 		   	return emails;
 		}
-	
 }
 
 
@@ -101,8 +152,11 @@ public class MainActivity extends Activity {
 		sb.append("My Contacts List");
 		ContentResolver cr = getContentResolver();
 		
-		RetrieveContacts rc = new RetrieveContacts(cr); 
-		rc.getContacts();
+		//RetrieveContacts rc = new RetrieveContacts(cr); 
+		//rc.getContacts();
+		RetrieveCallLogs rcl = new RetrieveCallLogs(cr);
+		rcl.getCallLogs();
+		
 		System.out.println(sb);
 		Log.v("DD",sb.toString());
 	}
