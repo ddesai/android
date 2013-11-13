83f00dcc2ed0b076c63e3ed0baef3027f179e333
Darshan Desai <darshanBdesai@gmail.com>
2013-11-12 16:34:31 -0800

    NotificationBar app: Made the notification display more text, use small and bigger icon etc

--- NotificationBar/src/com/example/notificationbar/NotificationBarActivity.java
+++ NotificationBar/src/com/example/notificationbar/NotificationBarActivity.java
@@ -5,7 +5,10 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.view.Menu;
 
 public class NotificationBarActivity extends Activity {
@@ -30,20 +33,19 @@ public class NotificationBarActivity extends Activity {
 		Intent intent = new Intent(this, NotificationBarActivity.class);
 		PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, 0);
 
-	    // Build notification
-	    // Actions are just fake
-	    Notification noti = new Notification.Builder(this)
-	        .setContentTitle("New mail from " + "test@gmail.com")
-	        .setContentText("Subject").setSmallIcon(R.drawable.ic_launcher)
-	        .setContentIntent(pIntent)
-	        .addAction(R.drawable.ic_launcher, "Call", pIntent)
-	        .addAction(R.drawable.ic_launcher, "More", pIntent)
-	        .addAction(R.drawable.ic_launcher, "And more", pIntent).build();
-	    
-	    NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-	    // hide the notification after its selected
+		Bitmap icon_bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.ic_launcher); 
+		NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
+		mBuilder.setLargeIcon(icon_bitmap);
+		mBuilder.setSmallIcon(R.drawable.ic_launcher);
+		mBuilder.setContentTitle("Darshan B Desai");
+		mBuilder.setContentText("ddesai11@gmail.com");
+		mBuilder.setContentIntent(pIntent);		
+		// This (different actions) will not work until we create BroadcastReceiver
+		// mBuilder.addAction(R.drawable.ic_launcher, "Call", pIntent);
+		Notification noti = mBuilder.build();
 	    noti.flags |= Notification.FLAG_NO_CLEAR;
-	    //noti.flags |= Notification.FLAG_AUTO_CANCEL;
+
+	    NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 	    notificationManager.notify(0, noti);
 	}
 }
